40678c0ac0df23cb188016863b2c023889d56501
Noor <nrandin@gmail.com>
2019-02-26 17:29:13 -0500

    Test

--- src/main/java/application/BuddyInfoRepository.java
+++ src/main/java/application/BuddyInfoRepository.java
@@ -2,11 +2,12 @@ package application;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "buddy", path = "buddy.json")
 public interface BuddyInfoRepository extends CrudRepository<BuddyInfo, String> {
     List<BuddyInfo> findAll();
-    List<BuddyInfo> findByName(String name);
+    List<BuddyInfo> findByName(@Param("name") String name);
 }
