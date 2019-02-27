4bb3e38da4e8461ec920ae0a9a4c3d7435dcca4b
Noor <nrandin@gmail.com>
2019-02-27 11:40:04 -0500

    tied up

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,13 @@
+package application;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Launcher {
+
+    public void launch(){
+        String[] contextPaths = new String[]{"META-INF/app-context.xml"};
+        new ClassPathXmlApplicationContext(contextPaths);
+    }
+}
+
---
+++ src/main/java/application/Runner.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Runner {
+
+    public static void main(String[] args){
+        Launcher launcher = new Launcher();
+        launcher.launch();
+
+        /*Lab 02*/
+        /*System.out.println("Running JPA example...\n\n");
+        JPATest biem = new JPATest();
+        biem.performJPA();*/
+    }
+}
+
